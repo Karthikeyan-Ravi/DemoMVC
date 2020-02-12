8d0f71d3b86fc9e42754a02924a9557f70e96028
Karthikeyan Ravi <karthikeyan.ravi11@hotmail.com>
2020-02-12 18:43:28 +0530

    DemoCommit

---
+++ DemoMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DemoMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Main",
+                url: "Page",
+                defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Sample",
+                url: "SamplePage",
+                defaults: new { controller = "Sample", action = "MainPage", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            
+        }
+    }
+}
